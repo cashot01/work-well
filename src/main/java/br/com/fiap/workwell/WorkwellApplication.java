5378e7e64f7c95f82fd6a6f3cf5e6c766bf928c7
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2025-11-10 11:31:41 -0300

    feat: create project spring boot gradle

---
+++ src/main/java/br/com/fiap/workwell/WorkwellApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.workwell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkwellApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkwellApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/workwell/WorkwellApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.workwell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkwellApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
