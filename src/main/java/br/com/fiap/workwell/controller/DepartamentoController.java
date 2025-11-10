b390b1f0521acbb192b7e1e98d2c1ba1981498b7
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2025-11-10 12:58:40 -0300

    feat: create Security e I18n

--- src/main/java/br/com/fiap/workwell/controller/DepartamentoController.java
+++ src/main/java/br/com/fiap/workwell/controller/DepartamentoController.java
@@ -1,12 +1,14 @@
 package br.com.fiap.workwell.controller;
 
 import br.com.fiap.workwell.model.Departamento;
+import br.com.fiap.workwell.service.DepartamentoService;
 import br.com.fiap.workwell.service.EmpresaService;
 import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/departamentos")
