9cdd153c546fcd0e0e3f077a333f63cc39b2b922
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2025-11-14 17:41:26 -0300

    feat: add Error Controller

---
+++ src/main/java/br/com/fiap/workwell/controller/ErrorController.java
@@ -0,0 +1,14 @@
+package br.com.fiap.workwell.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ErrorController {
+
+    @GetMapping("/acesso-negado")
+    public String acessoNegado() {
+
+        return "acesso-negado";
+    }
+}
